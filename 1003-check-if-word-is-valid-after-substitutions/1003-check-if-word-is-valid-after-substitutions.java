dc29ed02e5ac2e3d6b5f15fc68a10adbc4aae259
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-02-19 10:11:53 +0530

    Time: 13 ms (70.10%), Space: 42.2 MB (67.84%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0;i < s.length() ;i++){
+            char ch = s.charAt(i);
+            
+            if(ch == 'c'){
+                if(stk.size() >= 2  && stk.pop() == 'b' && stk.pop() == 'a'){
+                    
+                }else{
+                    return false;
+                }
+            }else{
+                stk.push(ch);
+            }
+        }
+        if (stk.size() == 0) return true;
+        
+        return false;
+           
+    }
+}
