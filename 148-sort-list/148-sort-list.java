8ddbd33a793eb8d16ee7e475f1956ec3b6eb6788
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-02-24 10:31:49 +0530

    Time: 6 ms (94.69%), Space: 49.8 MB (80.23%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middle(ListNode head){
+        ListNode slow = head , fast = head , prev = null;
+        while(fast!= null && fast.next != null){
+             prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            
+        }
+         if(fast == null)return prev;
+            
+            return slow;
+    }
+    public ListNode helper(ListNode head){
+         if(head == null || head.next == null){
+            return head;
+        }
+        ListNode mid = middle(head);
+        ListNode l2 = mid.next;
+        mid.next = null;
+        
+        ListNode left = helper(head);
+        ListNode right = helper(l2);
+        
+        return mergelist(left , right);
+    }
+    public ListNode mergelist(ListNode head1 , ListNode head2){
+        ListNode dummy = new ListNode(-1);
+        if(head1 == null) return head2;
+  if(head2 == null) return head1;
+        ListNode head = dummy , tail = dummy;
+        while(head1 != null && head2 != null){
+            if(head1.val < head2.val){
+               tail.next = head1;
+                head1 = head1.next;
+            }else{
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        
+        if(head1 != null){
+            tail.next = head1;
+        }
+         if(head2 != null){
+            tail.next = head2;
+        }
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {  
+        return helper(head);
+        
+    }
+}
