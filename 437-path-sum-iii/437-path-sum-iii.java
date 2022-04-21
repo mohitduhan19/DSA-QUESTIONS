1b2017f273e5e385da267a66e6e30688df6cbe23
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-04-22 00:59:23 +0530

    Time: 3 ms (91.57%), Space: 42.4 MB (76.60%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,19 +14,41 @@
  * }
  */
 class Solution {
-    int count = 0;
-    private void helper(TreeNode root , int sum){
-        if(root == null)return;
-        if(root.val == sum)count++;
+//     //TC - O(n^2) , SC - O(logh);
+//       int count = 0;
+//     private void helper(TreeNode root , int sum){
+//         if(root == null)return;
+//         if(root.val == sum)count++;
         
-        helper(root.left , sum - root.val);
-        helper(root.right , sum - root.val);
+//         helper(root.left , sum - root.val);
+//         helper(root.right , sum - root.val);
+//     }
+//     public int pathSum(TreeNode root, int target) {
+//         if(root == null)return 0;
+//         helper(root , target);
+//         pathSum(root.left , target);
+//         pathSum(root.right , target);
+//         return count;
+    
+    //Approach 2 : TC - O(n) , SC - O(n);
+   int ans;
+	public int pathSum(TreeNode root, int targetSum) {
+        Map<Integer, Integer> count = new HashMap();
+        count.put(0, 1);
+        solve(root, 0, targetSum, count);  
+        return ans;
     }
-    public int pathSum(TreeNode root, int target) {
-        if(root == null)return 0;
-        helper(root , target);
-        pathSum(root.left , target);
-        pathSum(root.right , target);
-        return count;
+    
+    void solve(TreeNode root, int currSum, int target, Map<Integer, Integer> count) {
+        if(root == null) return;
+        int sum = currSum + root.val;
+        if(count.containsKey(sum - target)) {
+            ans += count.get(sum - target);
+        }
+        count.put(sum, count.getOrDefault(sum, 0) + 1);
+        solve(root.left, currSum + root.val, target, count);
+        solve(root.right, currSum + root.val, target, count);
+        count.put(sum, count.get(sum) - 1);
+    
     }
 }
