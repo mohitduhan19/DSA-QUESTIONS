722a1b93bc0df22b9638aa5cf620b777a8f053e7
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-07-28 16:42:17 +0530

    Time: 137 ms (12.57%), Space: 104.4 MB (7.42%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,16 +1,38 @@
 class Solution {
+    class pair{
+        int left;
+        int right;
+        pair(int left ,  int right){
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public class MyComparator implements Comparator<pair>{
+        public int compare(pair obj1 , pair obj2){
+            if(obj1.right < obj2.right)return -1;
+            
+            return 1;
+        }
+    }
+    
     public int findMinArrowShots(int[][] points) {
-         Arrays.sort(points,(a , b)->Integer.compare(a[1],b[1]));
-         int count = 0; 
-         long limit = Long.MIN_VALUE;
-         
-         for(int i = 0; i < points.length;i++){
-             if(limit < points[i][0]){
-                 limit = points[i][1];
-                 count++;
-             }
-         }
-        // if(limit == Integer.MIN_VALUE)return 1;
+        
+        pair[] arrow = new pair[points.length];
+        for(int i = 0; i < points.length;i++){
+            arrow[i] = new pair(points[i][0] , points[i][1]);
+        }
+        
+        Arrays.sort(arrow , new MyComparator());
+        
+        int count = 1 ,limit = arrow[0].right;
+        for(int i = 1;i < arrow.length;i++){
+            if(limit < arrow[i].left){
+                count++;
+                limit = Math.max(limit , arrow[i].right);
+            }
+           
+        }
         return count;
     }
 }
