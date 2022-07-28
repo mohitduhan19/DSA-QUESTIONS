b358bf2647d0455af8467ea99004a31a7f8c62fe
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-07-28 15:10:13 +0530

    Time: 9 ms (99.91%), Space: 42.5 MB (90.23%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
@@ -1,15 +1,34 @@
 class Solution {
+    class pair{
+        int left;
+        int right;
+        pair(int left ,  int right){
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public class MyComparator implements Comparator<pair>{
+        public int compare(pair obj1 , pair obj2){
+            if(obj1.right != obj2.right){
+                return obj1.right - obj2.right;
+            }
+            return obj1.left - obj2.left;
+        }
+    }
     public int findLongestChain(int[][] pairs) {
-         Arrays.sort(pairs,(a , b)->Integer.compare(a[1],b[1]));
-         int count = 0; 
-         int limit = Integer.MIN_VALUE;
-         
-         for(int i = 0; i < pairs.length;i++){
-             if(limit < pairs[i][0]){
-                 limit = pairs[i][1];
-                 count++;
-             }
-         }
+       pair[]interval = new pair[pairs.length];
+       for(int i = 0;i < pairs.length;i++){
+           interval[i] = new pair(pairs[i][0] , pairs[i][1]);
+       } 
+        Arrays.sort(interval , new MyComparator());
+        int count = 0 , limit = Integer.MIN_VALUE;
+        for(int i = 0; i < interval.length;i++){
+            if(limit < interval[i].left){
+                count++;
+                limit = Math.max(limit , interval[i].right);
+            }
+        }
         return count;
     }
 }
