bc57f22fb16da2cd4dfc6a24a8adaad401b12103
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-08-13 01:41:57 +0530

    Time: 3 ms (81.45%), Space: 47.4 MB (40.88%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,25 +14,57 @@
  * }
  */
 class Solution {
-    TreeNode prev = null, first = null, second = null;
-
     public void recoverTree(TreeNode root) {
-        evalSwappedNodes(root);
-        int temp = first.val;
-        first.val = second.val;
-        second.val = temp;
-    }
-
-    private void evalSwappedNodes(TreeNode curr) {
-        if (curr == null)
-            return;
-        evalSwappedNodes(curr.left);
-        if (prev != null && prev.val > curr.val) {
-            if (first == null)
-                first = prev;
-            second = curr;
+        TreeNode curr = root;
+        TreeNode prev = null;
+        TreeNode n1 = null;
+        TreeNode n2 = null;
+        
+        while(curr != null){
+            if(curr.left == null){
+                //print
+                if(prev != null){
+                if(curr.val < prev.val){
+                    if(n1 == null){
+                        n1 = prev;
+                        n2 = curr;
+                    }else{
+                        n2 = curr;
+                    }
+                 }
+                }
+                prev = curr;
+                curr = curr.right;
+            }else{
+                TreeNode ipred = curr.left;
+                while(ipred.right != null && ipred.right != curr){
+                    ipred = ipred.right;
+                }
+                
+                if(ipred.right == null){
+                    ipred.right = curr;
+                    curr = curr.left;
+                }else{
+                    
+                    //print;
+                    if(prev != null){
+                if(curr.val < prev.val){
+                    if(n1 == null){
+                        n1 = prev;
+                        n2 = curr;
+                    }else{
+                        n2 = curr;
+                    }
+                 }
+                }
+                    prev = curr;
+                    ipred.right = null;
+                    curr = curr.right;
+                }
+            }
         }
-        prev = curr;
-        evalSwappedNodes(curr.right);
+        int temp = n1.val;
+        n1.val = n2.val;
+        n2.val = temp;
     }
 }
