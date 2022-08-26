f4a0abffd28299eddced5ac6034e88eb8a019aea
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-08-27 00:05:47 +0530

    Time: 4 ms (36.83%), Space: 106.3 MB (10.64%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -9,15 +9,16 @@
  */
 
 class Solution {
-    TreeNode res;
+     TreeNode res;
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        if(original != null){
-            getTargetCopy(original.left, cloned.left, target);
-            if(original == target){
-                res = cloned;
-            }
-            getTargetCopy(original.right, cloned.right, target);
+        if(original == null)return null;
+                if(original == target){
+            res  = cloned;
         }
+        getTargetCopy(original.left , cloned.left ,target);
+       
+        getTargetCopy(original.right , cloned.right , target);
+
         return res;
     }
 }
