b52eee1e4a01825d1e0b9e5c98261e249d5d6964
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-08-31 21:19:38 +0530

    Time: 1 ms (80.07%), Space: 43.5 MB (22.36%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,28 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-   public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-  if(head1 == null) return head2;
-  if(head2 == null) return head1;
-      
-   ListNode dummy = new ListNode(-1);
-  
-   ListNode head = dummy , tail = dummy;
-    while(head1 != null && head2 != null){
-        if(head1.val <= head2.val){
-            tail.next = head1;
-            head1 = head1.next;
-        }else{
-            tail.next = head2;
-            head2 = head2.next;
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        dummy.next = head1;
+        while(head1 != null && head2!=null){
+            if(head1.val <= head2.val){
+                tail.next = head1;
+                head1 = head1.next;
+            }else{
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
         }
-        tail = tail.next;
+        
+        if(head1 != null)tail.next = head1;
+        if(head2 != null)tail.next = head2;
+            return dummy.next;
     }
-    if(head1 != null){
-        tail.next = head1;
-    }else{
-        tail.next = head2;
-    }
-    return dummy.next;
-  }
-    
 }
- 
