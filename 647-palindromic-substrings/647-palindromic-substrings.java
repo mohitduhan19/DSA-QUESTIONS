c5209068d47be54cfb84014256ec0c88f1190fad
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-09-18 13:13:34 +0530

    Time: 36 ms (18.30%), Space: 44.3 MB (31.31%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,23 +1,21 @@
 class Solution {
-   public int countSubstrings(String s) {
-        if(s == null || s.length() == 0){
-            return 0;
-        }
-        int count = s.length();
-        int l;
-        int h;
-        for(int i = 0; i < s.length(); i++){
-		    // even
-            l = i;
-            h = i + 1;
-            while(l >= 0 && h < s.length() && s.charAt(l--) == s.charAt(h++)){
-                count++;
-            }
-			// odd
-            l = i - 1;
-            h = i + 1;
-            while(l >= 0 && h < s.length() && s.charAt(l--) == s.charAt(h++)){
-                count++;
+    public int countSubstrings(String s) {
+       boolean[][] dp = new boolean[s.length()][s.length()];
+        int count = 0;
+        for(int gp = 0; gp < s.length();gp++){
+            for(int i = 0 , j = gp; j < dp.length;i++,j++){
+                if(gp == 0){
+                    dp[i][j] = true;
+                }else if(gp == 1){
+                    dp[i][j] = s.charAt(i) == s.charAt(j)?true:false;
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i + 1][ j - 1] == true){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+                if(dp[i][j] == true)count++;
             }
         }
         return count;
