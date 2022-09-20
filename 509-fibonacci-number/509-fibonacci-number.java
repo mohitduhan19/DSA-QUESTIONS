50c7b5faa0a0d0324d17142064a0beda8bc239da
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-09-21 01:32:44 +0530

    Time: 0 ms (100.00%), Space: 41 MB (40.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,28 +1,6 @@
 class Solution {
-    //Memoization
-//     public int fibmem(int n ,  int[] dp){
-//         if(n == 0 || n == 1)return n;
-//         if(dp[n] != -1)return dp[n];
-        
-//         int fib1 = fibmem(n - 1 , dp);
-//         int fib2 = fibmem(n - 2, dp);
-//         dp[n] = fib1 + fib2;
-//         return dp[n];
-//     }
-    
-    // --------Recursive--->TC-O(2^n)-------
-          // public int fibrec(int n){
-//         if(n == 0 || n == 1)return n;
-//         int fib1 = fib(n - 1);
-//         int fib2 = fib(n - 2);
-        
-//         int ans = fib(n - 1) + fib(n - 2);
-//         return ans;
-//     }
-       public int fib(int n) {
-//         return fibrec(n);
-        
-        //--------------Tabulation -> TC-O(n)-----------
+    public int fib(int n) {
+        if(n ==0 ||n==1)return n;
         int[] dp = new int[n + 1];
            if(n == 0)return 0;
         dp[0] = 0 ; dp[1] = 1;
@@ -31,13 +9,5 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
-        
-        //-----------------Memoization---------------
-        
-        // int[] dp = new int[n + 1];
-        // Arrays.fill(dp , -1);
-        // fibmem(n , dp);
-        // return dp[n];
     }
-    
 }
