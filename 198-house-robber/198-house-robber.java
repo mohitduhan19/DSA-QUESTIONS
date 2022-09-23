7ed7ad5ca5bec7717dcce2ac8d8bd622274d80c7
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-09-23 23:26:47 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.03%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,25 +1,16 @@
 class Solution {
-    public int maxAmount(int[] nums, int idx , int lastHouse , int[][] dp){
-        if(idx >= nums.length)return 0;
-        if(dp[idx][lastHouse] != -1)return dp[idx][lastHouse];
-        if(lastHouse == 1){
-            int ans = maxAmount(nums , idx + 1 , 0 , dp);
-            dp[idx][lastHouse] = ans;
-            return ans;
+    private Integer[]dp;
+    public int helper(int[] nums , int curr){
+        if(curr >= nums.length)return 0;
+        if(dp[curr] != null){
+            return dp[curr]; 
         }
-        
-        int ansYes = nums[idx] + maxAmount(nums , idx + 1 , 1 , dp);
-        int ansNo = maxAmount(nums , idx + 1, 0 , dp);
-        int maxans = Math.max(ansYes , ansNo);
-        return maxans;
+        int steal = nums[curr] + helper(nums , curr + 2 );
+        int notSteal = helper(nums,  curr + 1);
+        return dp[curr] = Math.max(steal , notSteal);
     }
     public int rob(int[] nums) {
-        int lastHouse = 0;
-        int[][] dp = new int[nums.length + 1][2];
-        for(int i = 0; i < nums.length;i++){
-            Arrays.fill(dp[i] , -1);
-        }
-        int ans = maxAmount(nums , 0 , lastHouse , dp);
-        return ans;
+    dp = new Integer[nums.length];
+     return helper(nums , 0);
     }
 }
