80abdae8aeb1fd2cbede58a99f9e18e6a0da4efd
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-09-24 17:45:47 +0530

    Time: 1 ms (43.22%), Space: 41 MB (81.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,18 +1,20 @@
- class Solution {
-public int rob(int[] nums) {
-    if(nums.length == 1)return nums[0];
-	return Math.max(rob(nums, 0, nums.length-2), rob(nums, 1, nums.length-1));
-}
-
-public int rob(int[] nums, int lo, int hi) {
-    int preRob = 0, preNotRob = 0, rob = 0, notRob = 0;
-    for (int i = lo; i <= hi; i++) {
-      	rob = preNotRob + nums[i];
-    	notRob = Math.max(preRob, preNotRob);
-    	
-    	preNotRob = notRob;
-    	preRob = rob;
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp1 = new int[nums.length];
+        int[]dp2 = new int[nums.length];
+        if(nums.length == 1)return nums[0];
+        if(nums.length == 2)return Math.max(nums[0] , nums[1]);
+        
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0] , nums[1]);
+        
+        dp2[0] =0;
+        dp2[1] = nums[1];
+        
+        for(int i = 2; i < nums.length;i++){
+            dp2[i] = Math.max(nums[i] + dp2[i - 2] , dp2[i - 1]);
+            dp1[i] = Math.max(nums[i] + dp1[i - 2] , dp1[i - 1]);
+        }
+        return Math.max(dp1[nums.length - 2] , dp2[nums.length - 1]);
     }
-    return Math.max(rob, notRob);
-}
 }
