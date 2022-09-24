da3b9de3db74291e8fba1b4927ec8e31568b6bbe
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-09-25 00:46:49 +0530

    Time: 0 ms (100.00%), Space: 42 MB (91.91%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.java
+++ 337-house-robber-iii/337-house-robber-iii.java
@@ -14,26 +14,40 @@
  * }
  */
 class Solution {
-    class HousePair{
-        int withRoberry = 0;
-        int withoutRoberry = 0;
-    }
+    // class HousePair{
+    //     int withRoberry = 0;
+    //     int withoutRoberry = 0;
+    // }
     
-    public HousePair helper(TreeNode root){
-        if(root == null)return new HousePair();
+//     public HousePair helper(TreeNode root){
+//         if(root == null)return new HousePair();
         
-        HousePair left = helper(root.left);
-        HousePair right = helper(root.right);
+//         HousePair left = helper(root.left);
+//         HousePair right = helper(root.right);
         
-        HousePair ans = new HousePair();
+//         HousePair ans = new HousePair();
         
-        ans.withRoberry = left.withoutRoberry + root.val + right.withoutRoberry;
-        ans.withoutRoberry = Math.max(left.withoutRoberry , left.withRoberry) + 
-            Math.max(right.withoutRoberry , right.withRoberry);
+//         ans.withRoberry = left.withoutRoberry + root.val + right.withoutRoberry;
+//         ans.withoutRoberry = Math.max(left.withoutRoberry , left.withRoberry) + 
+//             Math.max(right.withoutRoberry , right.withRoberry);
+//         return ans;
+//     }
+    public int[] helper(TreeNode root){
+        if(root == null)return new int[2];
+        int[] ans = new int[2];
+        
+         int[] left = helper(root.left);
+         int[] right = helper(root.right);
+        
+        ans[0] = left[1] + root.val + right[1];
+        ans[1] = Math.max(left[0] , left[1]) + Math.max(right[0] , right[1]);
         return ans;
     }
     public int rob(TreeNode root) {
-        HousePair ans = helper(root);
-        return Math.max(ans.withoutRoberry , ans.withRoberry);
+        // HousePair ans = helper(root);
+        // return Math.max(ans.withoutRoberry , ans.withRoberry);
+        
+        int[] ans = helper(root);
+        return Math.max(ans[0] , ans[1]);
     }
 }
