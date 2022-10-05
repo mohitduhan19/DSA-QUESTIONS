fd0006128dec663b09507dd9206a7ce749621d21
mohitduhan19 <91405114+mohitduhan19@users.noreply.github.com>
2022-10-05 19:26:05 +0530

    Time: 34 ms (5.27%), Space: 48.8 MB (7.63%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] dp = new int[s.length()];
+        for(int i = 0; i < s.length();i++){
+            for(int j = 0; j <= i;j++){
+                String wordtoCheck = s.substring(j , i + 1);
+                if(wordDict.contains(wordtoCheck)){
+                if(j > 0){
+                    dp[i] += dp[j - 1];
+                }else{
+                    dp[i] += 1;
+                }
+                
+            }
+            }
+        }
+        return dp[s.length() - 1] > 0?true:false;
+    }
+}                                      
